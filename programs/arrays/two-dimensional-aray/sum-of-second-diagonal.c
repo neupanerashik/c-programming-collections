4417873690b531b12f954adb2572f4590bbfbede
Rashik Neupane <neupanerashik1@gmail.com>
2024-07-09 19:11:07 +0545

    array: two dimensional: diagonal: sum

---
+++ programs/arrays/two-dimensional-aray/sum-of-second-diagonal.c
@@ -0,0 +1,41 @@
+//Program to find the sum of second diagonal
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int i, j, sum = 0, mat[30][30], m, n;
+    printf("Enter the order of m*n:\n");
+    scanf("%d%d",&m, &n);
+    if(m==n)
+    {
+        printf("Enter the elements of matrix:\n");
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                scanf("%d", &mat[i][j]);
+            }
+        }
+        printf("\n The given matrix is:\n");
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                printf("%d\t", mat[i][j]);
+            }
+            printf("\n");
+        }
+        for(i=0; i<m; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                if((i+j == (m-1)) ) //for primary and secondary diagonal only this condition is changed
+                {
+                    sum = sum + mat[i][j];
+                }
+            }
+        }
+        printf("\nThe sum of second diagonal is:%d", sum);
+    }
+}
